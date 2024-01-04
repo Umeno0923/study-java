d92aaedcd4c8fd6b3e17da3323e9bfab7d007fdc
Umeno0923 <ihsasum0923@icloud.com>
2024-01-04 22:05:06 +0900

    StudyEnum

---
+++ studyJava1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Product.Orange);
+        System.out.println(Product.Apple);
+        System.out.println(Product.Cherry);
+    }
+}
